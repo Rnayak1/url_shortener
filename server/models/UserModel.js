f8b0c43bdc52a106c15469b5a35dd41a811cfc9c
rnayak1 <rohitn116@gmail.com>
2020-02-23 22:18:09 +0530

    verify api via postman

--- server/models/UserModel.js
+++ server/models/UserModel.js
@@ -1,29 +1,74 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
-module.exports = mongoose.model('userModel',new Schema({
-    username : {
-        type : String,
-        required : true
+const userModel = mongoose.model('userModel', new Schema({
+    username: {
+        type: String,
+        required: true
     },
-    email : {
-        type : String,
-        unique : true,
-        required : true
+    email: {
+        type: String,
+        unique: true,
+        required: true
     },
-    password : {
-        type : String,
+    password: {
+        type: String,
         required: true,
-        minlength : 6
+        minlength: 6
     },
-    contact : {
-        type : Number,
-        minlength : 10,
-        maxlength : 10,
-        required : true
+    contact: {
+        type: Number,
+        minlength: 10,
+        maxlength: 10,
+        required: true
     },
-    status : {
-        type : String,
-        default : 'inactive'
+    status: {
+        type: String,
+        default: 'inactive'
+    },
+    token: {
+        type: String,
+        required: true
     }
-}),'UserManager');
+}), 'UserManager');
+
+const loginModel = mongoose.model('loginModel', new Schema({
+    email: String,
+    password: String,
+    username: {
+        type: String,
+        required: false
+    }
+}), 'UserManager')
+
+const urlModel = mongoose.model('urlModel', new Schema({
+    orignalLink: {
+        type: String,
+        required: true
+    },
+    hashLink: {
+        type: String,
+        required: true
+    },
+    userId: {
+        type: String,
+        required: true
+    }
+}), 'UrlManager');
+
+const clickModel = mongoose.model('clickModel', new Schema({
+    hashLink: String,
+    ip: String,
+    city: String,
+    postal: Number,
+    isp: String,
+    lattitude: Number,
+    longitude: Number,
+}), 'ClickCapture');
+
+module.exports = {
+    userModel,
+    loginModel,
+    urlModel,
+    clickModel
+}
--- server/routes/api/RegisterUser.js
+++ server/routes/api/RegisterUser.js
@@ -1,85 +1,133 @@
 const express = require('express')
 const mongoose = require('mongoose')
-const crypto= require('crypto')
+const crypto = require('crypto')
 const ndoemailer = require('nodemailer')
 const router = express.Router();
 //connecting database
-const conn = mongoose.connect('mongodb://localhost/clickAssignment',{
-    useNewUrlParser : true,
-    useUnifiedTopology : true,
-    useCreateIndex : true
+const conn = mongoose.connect('mongodb://localhost/clickAssignment', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true
 })
-.then(res => {})
-.catch(err => console.log(err))
+    .then(res => { })
+    .catch(err => console.log(err))
 // configuring nodemailer
 
 const transporter = ndoemailer.createTransport({
-    pool : true,
-    host : "c04.tmdcloud.asia",
-    port : 465,
-    secure : true,
-    auth :{
-        user : "rohit@adsxeniumtechnology.com",
-        pass : "Rekoj@123"
+    pool: true,
+    host: "c04.tmdcloud.asia",
+    port: 465,
+    secure: true,
+    auth: {
+        user: "rohit@adsxeniumtechnology.com",
+        pass: "Rekoj@123"
     }
 });
 
 //import model
-const User = require('../../models/UserModel')
-
+const model = require('../../models/UserModel')
 //RegisterUser
-router.get('/register',async(req,res)=>{
+router.get('/register', async (req, res) => {
     console.log(req.body)
     res.send("hello");
-   /*const data = new User({
-       username : "Rohit",
-       email : "rohitn116@gmail.com",
-       password : "abc@123",
-       contact : 7827113162,
-       verifyToken : "abc"
-   });
-   await data.save()
-        .then((response) => res.send(response))
-        .catch(err => res.send(err));*/
+    /*const data = new User({
+        username : "Rohit",
+        email : "rohitn116@gmail.com",
+        password : "abc@123",
+        contact : 7827113162,
+        verifyToken : "abc"
+    });
+    await data.save()
+         .then((response) => res.send(response))
+         .catch(err => res.send(err));*/
 })
-//post
-router.post('/register',async (req,res)=>{
+
+//post Register
+router.post('/register', async (req, res) => {
     //generate token
+    let data = {}
     const token = crypto.randomBytes(10).toString('hex');
-    //configure mail
-    const mailOptions = {
-        from : "rohit@adsxeniumtechnology.com",
-        to : "rohitadsxenium@gmail.com",
-        subject : "Verify Your Account",
-        text : `this is token  ${token}`,
-        html : ` <div>
+    console.log(req.body);
+    const { user } = req.body;
+    const doExist = await model.loginModel.findOne({ email: user.email })
+    if (doExist) {
+        res.send({
+            error: "true",
+            message: "User already exist for this mail"
+        });
+    }
+    else {
+        user.status = "inactive";
+        user.token = token;
+        console.log(user)
+        //sendMail(req, token);
+        console.log(result)
+        res.send({
+            error: "false",
+            message: "Please verify your email id"
+        })
+    }
+    async function sendMail(req, token) {
+        //configure mail
+        const mailOptions = {
+            from: "rohit@adsxeniumtechnology.com",
+            to: "rohitadsxenium@gmail.com",
+            subject: "Verify Your Account",
+            html: ` <div>
                     please click verify button to Verify your account 
                     <br>
                     <center>
-                        <a href="http://${req.headers.host}/api/verify?token=${token}" style="button"> Verify</a>
+                        <a type="button" href="http://${req.headers.host}/verify?token=${token}" style="button"> Verify</a>
                     </center>
                 </div>`
-    };
-    //send mail
-    transporter.sendMail(mailOptions,(err,resp)=>{
-        if(err)
-            res.send(err)
-        else
-            res.send(resp);
-    })
+        };
+        //send mail
+        transporter.sendMail(mailOptions, (err, resp) => {
+            if (err)
+                return ({
+                    error: true,
+                    data: err
+                })
+            else
+                return ({
+                    error: false,
+                    data: resp
+                })
+        })
+    }
 });
 
 // VerifyUser
-router.get('/verify',async (req,res)=>{
-        
-    // transporter.verify((err,resp)=>{
-    //     if(err)
-    //         res.send(err)
-    //     else
-    //         res.send(resp);
-    // })
-    
-    
+router.get('/verify', async (req, res) => {
     res.send(req.query)
+})
+
+router.post('/login', async (req, res) => {
+    console.log(req.body);
+    const response = await model.loginModel.findOne({ email: req.body.user.email.toLowerCase() })
+    if (response && (response.password == req.body.user.password))
+        res.send({
+            errorStatus: false,
+            name: response.username,
+            id: response._id
+        });
+    else
+        res.send({
+            errorStatus: true,
+            error: " Invalid UserName/Password"
+        })
+});
+
+router.post('/generate', async (req, res) => {
+    console.log(req.body);
+    if (req.body.url.passed == '' || req.body.url.passed.length < 6) {
+        // generate url
+        req.body.url.passed = crypto.randomBytes(6).toString('hex')
+    }
+    console.log(req.body.url.orignal.toString());
+    res.send({
+        status: true,
+        shortUrl: req.body.url.passed
+    });
 })
 module.exports = router;
