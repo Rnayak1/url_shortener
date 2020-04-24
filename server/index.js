5d8b132b22d571942d017fb91bc93ecb5e5467ac
rnayak1 <rohitn116@gmail.com>
2020-04-24 18:24:49 +0530

    added cors

--- server/index.js
+++ server/index.js
@@ -9,12 +9,16 @@ const app = express();
 //middlleware
 app.use(bodyParser.json());
 app.use(cors());
-
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
 const api = require('./routes/api/RegisterUser')
-app.use('/api',api);
-app.all('*',(req,res)=>{
+app.use('/api', api);
+app.all('*', (req, res) => {
     res.sendStatus(404);
 })
 const port = process.env.PORT || 5000;
 
-app.listen(port,()=> console.log(`server on ${port}`));
+app.listen(port, () => console.log(`server on ${port}`));
